7289b45bf75093876ad09d68b1f5479e5e5d4649
Sam Meech-Ward <sam@meech-ward.com>
2020-04-05 20:56:20 -0700

    Add package json

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const fm = require('front-matter')
 const questionParser = require('./questionParser')
 const regexTester = require('./regexTester')
 
+const defaultSections = ["Answers", "Feedback", "Hint"]
+
 const text = fs.readFileSync(process.argv[2]).toString()
 
-const question = questionParser({text})
+const questionSections = questionParser({text})
 
+const title = Object.keys(questionSections).filter(heading => !defaultSections.includes(heading))
+console.log(title)
 const tests = []
-Object.keys(question.Answers.answerSections).forEach(key => {
-  const answer = question.Answers.answerSections[key]
+Object.keys(questionSections.Answers.answerSections).forEach(key => {
+  const answer = questionSections.Answers.answerSections[key]
   answer.tests.forEach(test => {
     tests.push({regexString: answer.regex.text, testString: test.text})
   })
