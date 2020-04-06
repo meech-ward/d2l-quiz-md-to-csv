f3a19c7b343c6534b9ad7aab3bf524fa6ef16b0b
Sam Meech-Ward <sam@meech-ward.com>
2020-04-05 22:24:33 -0700

    make sure files is always an array

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const marked = require('marked')
+const { fm, md } = require('./markdown')
+
 const fs = require('fs')
-const fm = require('front-matter')
-
-marked.setOptions({
-  highlight: function(code, lang, callback) {
-    // console.log(code, lang, callback)
-    return code.replace(/[\n\r]/g, '<br>')
-  }
-})
-
-const markdown = text => marked(text).replace(/(<pre>|<\/pre>|[\n\r]|<h2[\s\S]*h2>)/g, '').trim()
-
+const path = require('path')
 
 const questionParser = require('./questionParser')
 const regexTester = require('./regexTester')
@@ -18,47 +9,52 @@ const regexTester = require('./regexTester')
 const markdownSections = ["Feedback", "Hint"]
 const defaultSections = ["Answers", ...markdownSections]
 
-const text = fs.readFileSync(process.argv[2]).toString()
+const pathToFiles = process.argv[2]
+const lstat = fs.lstatSync(pathToFiles)
+const files = !lstat.isDirectory() ? [pathToFiles] : fs.readdirSync(pathToFiles).map(file => path.join(pathToFiles, file))
 
-const questionSections = questionParser({text})
+files.forEach(file => {
+  const text = fs.readFileSync(file).toString()
 
-if (process.argv[3]) {
-  const tests = []
-  Object.keys(questionSections.Answers.answerSections).forEach(key => {
-    const answer = questionSections.Answers.answerSections[key]
-    answer.tests.forEach(test => {
-      tests.push({regexString: answer.regex.text, testString: test.text})
+  const questionSections = questionParser({text})
+
+  // Run tests if true is passed in as the second argument
+  if (process.argv[3]) {
+    const tests = []
+    Object.keys(questionSections.Answers.answerSections).forEach(key => {
+      const answer = questionSections.Answers.answerSections[key]
+      answer.tests.forEach(test => {
+        tests.push({regexString: answer.regex.text, testString: test.text})
+      })
     })
-  })
-  regexTester(tests)
-  return
-}
+    regexTester(tests)
+    return
+  }
 
+  const title = Object.keys(questionSections).filter(heading => !defaultSections.includes(heading))[0]
 
-const title = Object.keys(questionSections).filter(heading => !defaultSections.includes(heading))[0]
+  const content = fm(text.trim()) 
+  const questionAttributes = content.attributes
 
-const content = fm(text.trim()) 
-const questionAttributes = content.attributes
-
-let csv = `
+  let csv = `
 NewQuestion,SA
 ID,${questionAttributes.ID}
 Title,${title}
-QuestionText,"${markdown(questionSections[title].text)}",HTML
+QuestionText,"${md(questionSections[title].text)}",HTML
 Points,${questionAttributes.Points}
 Difficulty,${questionAttributes.Difficulty}
 InputBox,${questionAttributes.InputBox[0]},${questionAttributes.InputBox[1]},
-Hint,"${markdown(questionSections['Hint'].text)}",HTML,
-Feedback,"${markdown(questionSections['Feedback'].text)}",HTML,
-`
+Hint,"${md(questionSections['Hint'].text)}",HTML,
+Feedback,"${md(questionSections['Feedback'].text)}",HTML,
+  `
 
-Object.keys(questionSections.Answers.answerSections).forEach(key => {
-  const answer = questionSections.Answers.answerSections[key]
-  csv +=
+  Object.keys(questionSections.Answers.answerSections).forEach(key => {
+    const answer = questionSections.Answers.answerSections[key]
+    csv +=
 `Answer,${key},"${answer.regex.text}",regexp
 `
+  })
+
+
+  console.log(csv)
 })
-
-
-
-console.log(csv)
